a5eb071ba1594d6c2a449c182af5dded9c599c6f
lenichzdarova <lenichzdarova@gmail.com>
2023-02-09 19:36:10 +0300

    Dices add MaxRoll method

--- Assets/Scripts/Dices.cs
+++ Assets/Scripts/Dices.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 public struct Dices
 {
     private const int MIN_ROLL_RESULT = 1;
-    public readonly int rolls;
-    public readonly int faces;
+    private int _rolls;
+    private int _faces;
 
     public Dices(int rolls, int faces)
     {
-        this.rolls = rolls;
-        this.faces = faces;
+        this._rolls = rolls;
+        this._faces = faces;
     }
     public int Roll()
     {
         int result = 0;        
         
-        for(int i = 0; i < rolls; i++)
+        for(int i = 0; i < _rolls; i++)
         {
-            result += Random.Range(MIN_ROLL_RESULT, faces +1);
+            result += Random.Range(MIN_ROLL_RESULT, _faces +1);
+        }
+        return result;
+    }
+    public int MaxRoll()
+    {
+        int result = 0;
+
+        for (int i = 0; i < _rolls; i++)
+        {
+            result += _faces;
         }
         return result;
     }
 
     public override string ToString()
     {
-        return $"{rolls}d{this.faces}";
+        return $"{_rolls}d{this._faces}";
     }
 }
