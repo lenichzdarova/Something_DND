e5f30a9e77516c0676a8776fe0c42b80ea847c37
lenichzdarova <lenichzdarova@gmail.com>
2023-02-09 19:34:01 +0300

    Update BaseHealthBonusProvider.cs
    because of Dices struct implemented

--- Assets/Scripts/BaseHealthBonusProvider.cs
+++ Assets/Scripts/BaseHealthBonusProvider.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BaseHealthBonusProvider 
 {
-    public static (int,int) GetHealth(ClassEnum characterClass)
+    public static Dices GetHealth(ClassEnum characterClass)
     {
         return characterClass switch
         {
-            ClassEnum.Fighter => (1,12),
-            ClassEnum.Rogue => (1, 4),
-            ClassEnum.Wizard => (1, 6),
+            ClassEnum.Fighter => new Dices(1,12),
+            ClassEnum.Rogue => new Dices(1, 4),
+            ClassEnum.Wizard => new Dices(1, 6),
             _ => throw new NotImplementedException($"Character class {characterClass} health bonus not implemented")
         };
     }
