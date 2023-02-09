36f7ab5e18c4910b55f5d20615735375c870819d
lenichzdarova <lenichzdarova@gmail.com>
2023-02-09 18:48:27 +0300

    BaseHealthBonusProvider class

---
+++ Assets/Scripts/BaseHealthBonusProvider.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using UnityEngine;
+
+public class BaseHealthBonusProvider 
+{
+    public static (int,int) GetHealth(ClassEnum characterClass)
+    {
+        return characterClass switch
+        {
+            ClassEnum.Fighter => (1,12),
+            ClassEnum.Rogue => (1, 4),
+            ClassEnum.Wizard => (1, 6),
+            _ => throw new NotImplementedException($"Character class {characterClass} health bonus not implemented")
+        };
+    }
+}
