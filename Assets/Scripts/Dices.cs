f940e05dd978733f33d722d8120ac60531d46d20
lenichzdarova <lenichzdarova@gmail.com>
2023-02-09 19:32:50 +0300

    Dices struct

---
+++ Assets/Scripts/Dices.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public struct Dices
+{
+    private const int MIN_ROLL_RESULT = 1;
+    public readonly int rolls;
+    public readonly int faces;
+
+    public Dices(int rolls, int faces)
+    {
+        this.rolls = rolls;
+        this.faces = faces;
+    }
+    public int Roll()
+    {
+        int result = 0;        
+        
+        for(int i = 0; i < rolls; i++)
+        {
+            result += Random.Range(MIN_ROLL_RESULT, faces +1);
+        }
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{rolls}d{this.faces}";
+    }
+}
