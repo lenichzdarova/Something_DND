60e18e993afbcf5fae986a70b4b98167af4139c2
lenichzdarova <lenichzdarova@gmail.com>
2023-02-09 18:48:41 +0300

    LevelProgress class

---
+++ Assets/Scripts/LevelProgress.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LevelProgress 
+{
+    private int _level;
+    private int _experience;
+
+    public void AddExperience(int value)
+    {
+        _experience += value;
+        if(_experience > ExperienceTableProvider.ExperienceToNextLevel(_level))
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+
+    }
+}
