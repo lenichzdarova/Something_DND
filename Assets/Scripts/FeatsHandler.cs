514fc19026643e18181f57a171c5e6a6e5a2e6b4
lenichzdarova <lenichzdarova@gmail.com>
2023-02-11 01:17:44 +0300

    Add FeatsHandler class
    Incapsulate all character feats manipulations

---
+++ Assets/Scripts/FeatsHandler.cs
@@ -0,0 +1,69 @@
+﻿
+using System.Collections.Generic;
+
+public class FeatsHandler
+{
+    private List<Feat> _feats;
+
+    public FeatsHandler(RaceEnum race, ClassEnum characterClass)//fists lvl character constructor
+    {
+        int characterLevel = 1;
+        _feats = new List<Feat>();
+        _feats.AddRange(RaceFeatsProvider.GetFeats(race));
+        _feats.AddRange(ClassFeatsProvider.GetFeats(characterClass, characterLevel));
+    }
+
+    public List<IAbilitiesProvider> GetAbilitiesFeats() 
+    { 
+        List<IAbilitiesProvider> result = new List<IAbilitiesProvider>();
+        foreach (Feat feat in _feats)
+        {
+            if(feat is IAbilitiesProvider provider)
+            {
+                result.Add(provider);
+            }
+        }
+        return result;
+    }
+
+    public List<IAttackProvider> GetAttackFeats()
+    {
+        List<IAttackProvider> result = new List<IAttackProvider>();
+        foreach (Feat feat in _feats)
+        {
+            if (feat is IAttackProvider provider)
+            {
+                result.Add(provider);
+            }
+        }
+        return result;
+    }
+
+    public List<ISavingThrowsProvider> GetSavingThrowFeats()
+    {
+        List<ISavingThrowsProvider> result = new List<ISavingThrowsProvider>();
+        foreach (Feat feat in _feats)
+        {
+            if (feat is ISavingThrowsProvider provider)
+            {
+                result.Add(provider);
+            }
+        }
+        return result;
+    }
+
+    public List<IDamageProvider> GetDamageFeats()
+    {
+        List<IDamageProvider> result = new List<IDamageProvider>();
+        foreach (Feat feat in _feats)
+        {
+            if (feat is IDamageProvider provider)
+            {
+                result.Add(provider);
+            }
+        }
+        return result;
+    }
+
+
+}
