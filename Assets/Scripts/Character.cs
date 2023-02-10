7908ad20e1d2a5a751d89d514c711f23b1e81d9e
lenichzdarova <lenichzdarova@gmail.com>
2023-02-11 01:17:50 +0300

    Update Character.cs

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,6 +1,8 @@
 
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 /// <summary>
 ///Chracter class problems
@@ -8,7 +10,7 @@ using UnityEngine;
 /// This should not affect perfomance but looks shitty.
 /// </summary>
 
-public class Character : IInventoryProvider, IAttackProvider, IDamageProvider, ISavingThrowsProvider, IAbilitiesProvider, ICharacterFeatsProvider
+public class Character : IInventoryProvider, IAttackProvider, IDamageProvider, ISavingThrowsProvider, IAbilitiesProvider
 {
     private RaceEnum _characterRace;
     private ClassEnum _characterClass;
@@ -16,27 +18,29 @@ public class Character : IInventoryProvider, IAttackProvider, IDamageProvider, I
     public Sprite Portrait { get; private set; }    
 
     public LevelProgress _levelProgress;
-    private readonly Health _health;  
+    private readonly Health _health;
+
+    private FeatsHandler _featsHandler;
+
     
-    public Character(RaceEnum race, ClassEnum characterClass, Abilities abilities)
+    
+    public Character(RaceEnum race, ClassEnum characterClass, Abilities abilities) //first lvl character constructor
     {
         _characterRace = race;
         _characterClass = characterClass;
         _abilities = abilities;
         Portrait = default;
-        _health = new Health(characterClass,abilities);
+        _featsHandler = new FeatsHandler(race, characterClass);
+        _health = new Health(characterClass,abilities);                
     }
 
     public Abilities GetAbilities()
     {
         Abilities result = _abilities;
-        var feats = GetCharacterFeats();
+        var feats = _featsHandler.GetAbilitiesFeats();
         foreach ( var feat in feats )
         {
-            if (feat is IAbilitiesProvider provider)
-            {
-                result += provider.GetAbilities();
-            }
+            result += feat.GetAbilities();
         }
         //sum IabilitiesProvider from effects and feats
         return result;
@@ -48,15 +52,11 @@ public class Character : IInventoryProvider, IAttackProvider, IDamageProvider, I
         attackBonus += BaseAttackBonusProvider.GetAttackBonus(_characterClass, 
             _levelProgress.CurrentLevel);
         attackBonus += AbilitiesAttackBonusProvider.GetAttackBonus(GetAbilities());
-        var feats = GetCharacterFeats();
+        var feats = _featsHandler.GetAttackFeats();
         foreach (var feat in feats)
         {
-            if (feat is IAttackProvider provider)
-            {
-                attackBonus += provider.GetAttack();
-            }
-        }
-        //here we check for IAttackProvider effects and apply it        
+            attackBonus += feat.GetAttack();
+        }               
         return attackBonus;
     }
 
@@ -72,13 +72,11 @@ public class Character : IInventoryProvider, IAttackProvider, IDamageProvider, I
     public SavingThrows GetSavingThrows()
     {
         SavingThrows result = new SavingThrows();
+        var feats = _featsHandler.GetSavingThrowFeats();
+        foreach (var feat in feats)
+        {
+            result += feat.GetSavingThrows();
+        }
         return result;
-    }
-
-    public List<CharacterFeat> GetCharacterFeats()
-    {
-        List<CharacterFeat> feats = RaceFeatsProvider.GetFeats(_characterRace);
-        feats.AddRange(ClassFeatsProvider.GetFeats(_characterClass, _levelProgress.CurrentLevel));
-        return feats;
-    }
+    }    
 }
