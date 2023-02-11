d750bb268f751b8b5b41a4ffab62bd7b12829511
lenichzdarova <lenichzdarova@gmail.com>
2023-02-11 10:43:10 +0300

    AbilitiesHandler

---
+++ Assets/Scripts/AbilitiesHandler.cs
@@ -0,0 +1,24 @@
+﻿
+public class AbilitiesHandler
+{
+    private FeatsHandler _featsHandler;
+    private Abilities _baseAbilities;
+
+    public AbilitiesHandler(Abilities baseAbilities, FeatsHandler featsHandler)
+    {
+        _baseAbilities= baseAbilities;
+        _featsHandler= featsHandler;
+    }
+
+    public Abilities GetAbilities()
+    {
+        Abilities result = _baseAbilities;
+        var feats = _featsHandler.GetAbilitiesFeats();
+        foreach (var feat in feats)
+        {
+            result += feat.GetAbilities();
+        }
+        //sum IabilitiesProvider from effects and feats
+        return result;
+    }
+}
