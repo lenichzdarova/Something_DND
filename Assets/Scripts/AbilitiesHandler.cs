eb34681f7a53a530e14390e3de6a1221cc1adc31
lenichzdarova <lenichzdarova@gmail.com>
2023-02-11 11:35:42 +0300

    Update AbilitiesHandler.cs

--- Assets/Scripts/AbilitiesHandler.cs
+++ Assets/Scripts/AbilitiesHandler.cs
@@ -1,16 +1,33 @@
 ﻿
+using System;
+
 public class AbilitiesHandler
 {
+    public event Action Changed;
+
     private FeatsHandler _featsHandler;
-    private Abilities _baseAbilities;
+    private Abilities _baseAbilities; //pure value without any effects
+    private Abilities _finalValue;
 
     public AbilitiesHandler(Abilities baseAbilities, FeatsHandler featsHandler)
     {
         _baseAbilities= baseAbilities;
         _featsHandler= featsHandler;
+        _featsHandler.Changed += OnChange;
+        Calculate();
+    }
+
+    public void AddAbilities(Abilities abilities)
+    {
+        _baseAbilities += abilities;
+        Changed?.Invoke();
     }
 
     public Abilities GetAbilities()
+    {        
+        return _finalValue;
+    }
+    private void Calculate()
     {
         Abilities result = _baseAbilities;
         var feats = _featsHandler.GetAbilitiesFeats();
@@ -19,6 +36,12 @@ public class AbilitiesHandler
             result += feat.GetAbilities();
         }
         //sum IabilitiesProvider from effects and feats
-        return result;
+        _finalValue = result;
+    }
+
+    private void OnChange()
+    {
+        Calculate();
+        Changed?.Invoke();
     }
 }
